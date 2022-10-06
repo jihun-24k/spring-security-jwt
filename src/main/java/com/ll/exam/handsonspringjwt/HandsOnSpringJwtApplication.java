0d8ea592fff2de07de48b607811b2da261224d04
jihun-24k <gi020477@gmail.com>
2022-10-06 13:22:43 +0900

    Chore: 세팅

---
+++ src/main/java/com/ll/exam/handsonspringjwt/HandsOnSpringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.handsonspringjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnSpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnSpringJwtApplication.class, args);
+	}
+
+}
