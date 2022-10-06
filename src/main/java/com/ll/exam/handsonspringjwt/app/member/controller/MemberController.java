e13c1d8f4c63688cc2f547a68dea2d6fe93ddc0c
jihun-24k <gi020477@gmail.com>
2022-10-07 06:22:16 +0900

    Feat: 유효성 검사

--- src/main/java/com/ll/exam/handsonspringjwt/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/handsonspringjwt/app/member/controller/MemberController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authentication", "JWT키");
 
@@ -26,5 +30,9 @@ public class MemberController {
     public static class LoginDto {
         private String username;
         private String password;
+
+        public boolean isNotValid() {
+            return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
+        }
     }
 }
